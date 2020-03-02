b017c22004f85371068ec9d8d3d039296422859f
Yong Wen Chua <lawliet89@users.noreply.github.com>
2020-03-02 10:03:14 +0800

    Upgrade to async-await

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,15 @@
 use yup_oauth2 as oauth2;
 
-use failure::Fail;
-use futures::future::Future;
-use hyper::Client;
-use hyper_tls::{self, HttpsConnector};
-use oauth2::{GetToken, ServiceAccountAccess};
-use serde_json;
-
-#[derive(Debug, Fail)]
+#[derive(Debug, failure::Fail)]
 pub enum Error {
     #[fail(display = "Missing GOOGLE_CREDENTIALS")]
     MissingKey,
     #[fail(display = "IO Error: {:#?}", _0)]
     IOError(std::io::Error),
-    #[fail(display = "{}", _0)]
-    GenericError(String),
     #[fail(display = "JSON Error: {}", 0)]
     JsonError(serde_json::Error),
-    #[fail(display = "TLS Error: {}", 0)]
-    TlsError(#[cause] hyper_tls::Error),
+    #[fail(display = "OAuth Error: {}", 0)]
+    OauthError(yup_oauth2::error::Error),
 }
 
 impl From<std::io::Error> for Error {
@@ -33,46 +24,34 @@ impl From<serde_json::Error> for Error {
     }
 }
 
-impl From<hyper_tls::Error> for Error {
-    fn from(err: hyper_tls::Error) -> Error {
-        Error::TlsError(err)
+impl From<yup_oauth2::error::Error> for Error {
+    fn from(err: yup_oauth2::error::Error) -> Self {
+        Error::OauthError(err)
     }
 }
 
-fn main() -> Result<(), Error> {
+#[tokio::main]
+async fn main() -> Result<(), Error> {
     env_logger::init();
 
     let key_file = std::env::var_os("GOOGLE_CREDENTIALS")
         .ok_or_else(|| Error::MissingKey)?
         .to_string_lossy()
         .into_owned();
-    let key = oauth2::service_account_key_from_file(&key_file)?;
+    let key = oauth2::read_service_account_key(&key_file).await?;
 
-    let https = HttpsConnector::new(1)?;
-    let client = Client::builder().keep_alive(false).build(https);
+    let authenticator = oauth2::ServiceAccountAuthenticator::builder(key)
+        .build()
+        .await?;
 
-    let mut access = ServiceAccountAccess::new(key, client);
-    let token = access
-        .token(["https://www.googleapis.com/auth/cloud-platform"].iter())
-        .map_err(|e| {
-            let e = Error::GenericError(format!("Error getting token: {}", e));
-            println!("{}", e);
-        })
-        .and_then(|token| {
-            let json = serde_json::to_string_pretty(&token);
-            match json {
-                Ok(json) => {
-                    println!("{}", json);
-                    Err(())
-                }
-                Err(e) => {
-                    println!("{:#?}", e);
-                    Ok(())
-                }
-            }
-        });
+    // let https = HttpsConnector::new(1)?;
+    // let client = Client::builder().keep_alive(false).build(https);
 
-    tokio::run(token);
+    let token = authenticator
+        .token(&["https://www.googleapis.com/auth/cloud-platform"])
+        .await?;
 
+    let json = serde_json::to_string_pretty(&token)?;
+    println!("{}", json);
     Ok(())
 }
